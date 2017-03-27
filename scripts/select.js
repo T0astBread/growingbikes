63da35e0864385c40b54a219ace1cecb9ad5af3d
T0astBread <m.briedl@live.at>
2017-03-27 03:49:04 +0200

    Changes in Products Page and New News Page
    I have started working on a news page now. (the title of this branch is
    misleading, I know.) Also, I made some more changes to the CSS structure
    and added some placeholder images.

--- scripts/select.js
+++ scripts/select.js
@@ -2,11 +2,19 @@
  * Created by T0astBread on 19.03.2017.
  */
 
+
+let updateSelectWidth = (selectJQueryElm) =>
+{
+    $("#dynamic-size-select-temp").find("*").html(selectJQueryElm.find(":selected").html());
+    selectJQueryElm.width($("#dynamic-size-select-temp").width() + 10);
+};
+
 $(document).ready(() =>
-    $("select").change((e) =>
+{
+    $("select.dynamic-size-select").change((e) =>
     {
-        let tar = $(e.currentTarget), elm = tar.find(":selected"), prev = tar.html();
-        tar.html($("<span>" + elm.text()) + "</span>");
-        console.log();
-        $(tar).html(prev);
-    }));
+        updateSelectWidth($(e.currentTarget));
+    });
+
+    updateSelectWidth($("select:not('#dynamic-size-select-temp')"));
+});
